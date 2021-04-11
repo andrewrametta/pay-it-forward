f52fa217bb8f1d001063e616d1a732617d39888e
Andrew <ramettaandrewj@gmail.com>
2021-04-11 15:36:09 -0400

    about component

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Login from "./components/Login/Login";
 import Register from "./components/Register/Register";
 import Dashboard from "./components/Dashboard/Dashboard";
 import NewDonation from "./components/NewDonation/NewDonation";
+import About from "./components/About/About";
 import "./App.css";
 
 function App() {
@@ -19,6 +20,7 @@ function App() {
       <Route path="/login" component={Login} />
       <Route path="/newdonation" component={NewDonation} />
       <Route path="/dashboard" component={Dashboard} />
+      <Route path="/about" component={About} />
     </div>
   );
 }
---
+++ src/components/About/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function About() {
+  return (
+    <div className="about-wrapper">
+      <h1>About Page</h1>
+      <p>this is where all the cool stuff will live</p>
+    </div>
+  );
+}
+
+export default About;
