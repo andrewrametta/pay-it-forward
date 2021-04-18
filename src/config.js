b198c475e58c89f03d3c3d7c353dc701bb841fce
Andrew <ramettaandrewj@gmail.com>
2021-04-18 13:27:53 -0400

    added auth service and post and login user

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
   PORT: process.env.PORT || 8000,
   TOKEN_KEY: "pay-it-foward-token",
-  API_ENDPOINT: "http://localhost:8000/api",
+  API_ENDPOINT: "http://localhost:8000",
 };
---
+++ src/services/auth-api-service.js
@@ -0,0 +1,29 @@
+import config from "../config";
+import TokenService from "./token-service";
+
+export default {
+  // used to register user
+  postUser(user) {
+    return fetch(`${config.API_ENDPOINT}/api/users`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(user),
+    }).then((res) =>
+      !res.ok ? res.json().then((e) => Promise.reject(e)) : res.json()
+    );
+  },
+  // used to login user by matching user in database
+  loginUser(user) {
+    return fetch(`${config.API_ENDPOINT}/api/auth/login`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(user),
+    }).then((res) =>
+      !res.ok ? res.json().then((e) => Promise.reject(e)) : res.json()
+    );
+  },
+};
