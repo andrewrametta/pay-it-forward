4009862231a0a20455fa1ca5aedce80ac659af45
Andrew <ramettaandrewj@gmail.com>
2021-05-22 19:55:59 -0400

    added landing page

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -6,15 +6,22 @@ function Landing() {
   return (
     <div className="landing-wrapper">
       <h1>Pay it Forward</h1>
-
+      <h2>Why join?</h2>
       <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-        mollit anim id est laborum.
+        Have you ever had a piece of furniture you want to donate. Maybe you are
+        moving or down sizing and have boxes of things you want to go to a good
+        home. You open up google and have no clue where to start. Or maybe you
+        put it on the side of the road hoping someone will pick it up and then
+        it gets ruined by rain or sun?
+      </p>
+      <h2>What does this app do?</h2>
+      <p>
+        Pay it forward is a network you can join that is connected to verified
+        non profit organizations in your area. We take all of the guess work out
+        for you. Just sign in, post a picture of what you want to donate, and we
+        will connect you with the non profits in your area that can take your
+        donation. We make it simple and easy for you to find a good home for the
+        things that you want to get rid of.
       </p>
     </div>
   );
