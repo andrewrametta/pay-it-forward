4c31548e4c79e50c262a72b8c8f6179c3f9205b6
Andrew <ramettaandrewj@gmail.com>
2021-05-25 11:38:36 -0400

    connected heroku

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export default {
   PORT: process.env.PORT || 8000,
   TOKEN_KEY: "pay-it-foward-token",
-  //API_ENDPOINT: "https://fathomless-dawn-52857.herokuapp.com",
-  API_ENDPOINT: "http://localhost:8800",
+  API_ENDPOINT: "https://fathomless-dawn-52857.herokuapp.com",
+  //API_ENDPOINT: "http://localhost:8800",
 };
