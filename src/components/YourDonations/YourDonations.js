22b13426051d786df74d3d465378ae1d5c31acc1
Andrew <ramettaandrewj@gmail.com>
2021-06-10 16:42:10 -0400

    added past donations

--- src/components/YourDonations/YourDonations.js
+++ src/components/YourDonations/YourDonations.js
@@ -24,6 +24,10 @@ export default function YourDonations(props) {
       item.user_id === parseInt(userId) && item.cur_status === "available"
   );
 
+  const yourPastDonations = items.filter(
+    (item) => item.user_id === parseInt(userId) && item.cur_status === "claimed"
+  );
+
   return (
     <div className="yourdonations-container">
       <div className="yourdonations-header">
@@ -35,6 +39,39 @@ export default function YourDonations(props) {
         <ul className="yourdonations-items">
           {yourDonations.length > 0 ? (
             yourDonations
+              .sort((a, b) => a.id - b.id)
+              .map((filteredItem, indx) => (
+                <Link key={indx} to={`donation/${filteredItem.id}`}>
+                  <li key={indx} className="your-items-div-container">
+                    <div className="your-item-img">
+                      {filteredItem.item_url ? (
+                        <Image
+                          className="img-yourdonation"
+                          cloudName="hq1rpt94r"
+                          publicId={`${filteredItem.item_url}`}
+                          width="250"
+                          height="250"
+                          crop="fill"
+                        />
+                      ) : null}
+                    </div>
+                    <h3>{filteredItem.title}</h3>
+                    <p>{filteredItem.cur_status}</p>
+                  </li>
+                </Link>
+              ))
+          ) : (
+            <h3>Looks like you have no donations posted...</h3>
+          )}
+        </ul>
+      </div>
+      <div className="yourdonations-header">
+        {yourPastDonations.length > 0 && <h1>Donated Items</h1>}
+      </div>
+      <div className="your-items-container">
+        <ul className="yourdonations-items">
+          {yourPastDonations.length > 0 ? (
+            yourPastDonations
               .sort((a, b) => a.id - b.id)
               .map((filteredItem, indx) => (
                 <Link key={indx} to={`donation/${filteredItem.id}`}>
